addb4f4aeb0a459d10e853c4b4073ea4a197e9fd
visrahane <rahane@usc.edu>
2018-02-21 13:59:13 -0800

    add accuracy calculation script

---
+++ hmm/accuracycalculator.py
@@ -0,0 +1,26 @@
+correct=0;
+incorrect=0;
+line=1;
+with open("hmmoutput.txt",encoding="utf8") as textfile1, open("en_dev_tagged.txt",encoding="utf8") as textfile2:
+    for x, y in zip(textfile1, textfile2):
+        #print (x);
+        myLine = x.rstrip().split(" ");
+        expectedLine = y.rstrip().split(" ");
+        for myWord,expectedWord in zip(myLine,expectedLine):
+            myTags = myWord.split("/");
+            myTags[0:-1] = ["/".join(myTags[0:-1])];
+
+            expectedTags = expectedWord.split("/");
+            expectedTags[0:-1] = ["/".join(expectedTags[0:-1])];
+
+            if(expectedTags[1]!=myTags[1]):
+                incorrect+=1;
+                print("line-",line,"word:",expectedTags[0],myTags[0],"myTag:",myTags[1],"expectedTag:",expectedTags[1]);
+            else:
+                correct+=1;
+                #print("good");
+        line+=1;
+print("Correct:",correct);
+print("InCorrect:",incorrect);
+print("Total:",correct+incorrect);
+print("Accuracy:",correct/(correct+incorrect));
